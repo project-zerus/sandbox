288f2a5ce08f154e09a5886744832509924417d4
liuhuahang <liuhuahang@zerus.co>
2014-06-06 19:41:04 +0800

    add jsoncpp tests
    N/A
    
    Change-Id: I5f4efbe55541b2e0788183e398da5c3fd56636bd
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

---
+++ jsoncpp_tests/Tests.cpp
@@ -0,0 +1,10 @@
+
+#include "jsoncpp/json.h"
+
+#include "thirdparty/gtest/gtest.h"
+
+TEST(JsonCpp, Const) {
+  const Json::Value json;
+  const Json::Value& jsonApi = json["api"];
+  EXPECT_EQ(true, jsonApi.isNull());
+}
