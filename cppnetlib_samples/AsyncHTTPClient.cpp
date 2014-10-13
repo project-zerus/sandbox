20efd6f57c08a883824fb957cfe2ccf6d21b4f90
liuhuahang <liuhuahang@zerus.co>
2014-10-13 21:21:06 +0800

    demonstrate the threading behavior of cpp netlib
    N/A
    
    Change-Id: I31c9d9cbafd85707f3ecad4e458fe4fdb54dd00e
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

--- cppnetlib_samples/AsyncHTTPClient.cpp
+++ cppnetlib_samples/AsyncHTTPClient.cpp
@@ -13,9 +13,13 @@ int main(int argc, char** argv) {
   using boost::iterator_range;
   using boost::network::http::client;
   using boost::system::error_code;
+  std::thread::id threadID = std::this_thread::get_id();
+  std::cout << "Main thread #" << threadID << std::endl;
   boost::shared_ptr<io_service> ioService(new io_service());
   std::thread ioThread(
     [ioService] () {
+      std::thread::id threadID = std::this_thread::get_id();
+      std::cout << "IO thread #" << threadID << std::endl;
       try {
         boost::asio::io_service::work sentinel(*ioService);
         ioService->run();
@@ -25,15 +29,17 @@ int main(int argc, char** argv) {
     }
   );
   client::options options;
-  options.follow_redirects(true).cache_resolved(true).io_service(ioService);
+  options.io_service(ioService);
   client httpClient(options);
-  client::request request("http://www.xiaomi.com/");
+  client::request request("http://z1.huahang.im/");
   client::response response = httpClient.get(
     request,
     [&httpClient, ioService]
     (iterator_range<char const*> const& range, error_code const& code) {
+      std::thread::id threadID = std::this_thread::get_id();
+      std::cout << "Callback thread #" << threadID << std::endl;
       if (code == boost::system::errc::success) {
-        std::cout << ">>>>> DATA ";
+        std::cout << ">>>>> DATA" << std::endl;
         for (uint64_t i = 0; i < range.size(); ++i) {
           std::cout << range[i];
         }
@@ -46,7 +52,8 @@ int main(int argc, char** argv) {
       }
     }
   );
-  std::cout << ">>>>> REQUEST" << std::endl;
+  auto status = boost::network::http::status(response);
+  std::cout << ">>>>> REQUEST status " << status << std::endl;
   ioThread.join();
   std::cout << ">>>>> DONE" << std::endl;
   return 0;
